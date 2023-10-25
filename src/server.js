d57753c3930055af189bacb0909045c883ad041a
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-10-25 10:16:46 +0100

    setup cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 require('dotenv').config();
 
 
@@ -8,6 +9,8 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+
+app.use(cors());
 
 // Connect to your MongoDB database
 mongoose.connect(process.env.DB, {
