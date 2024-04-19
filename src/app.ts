b7a0800551479dfc9f472f59125fac3109a8088f
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2024-04-19 21:09:56 +0100

    root route

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
-import express from "express";
+import express, { Request, Response } from "express";
+// import { Request, Response } from "express";
 import bodyParser from "body-parser";
 import connectDB from "./database/config";
 import route from "./module/route/routes";
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("product api");
+});
 app.use(route);
 const startServer = async () => {
   try {
